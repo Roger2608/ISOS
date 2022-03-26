792f488734c50c10311ff63d22cdf2700324a3f9
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2022-03-25 23:30:29 -0500

    add commit initial

---
+++ isos_lss_model/src/main/java/com/isos/lss/app/model/IsosLssModelApplication.java
@@ -0,0 +1,13 @@
+package com.isos.lss.app.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsosLssModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsosLssModelApplication.class, args);
+	}
+
+}
