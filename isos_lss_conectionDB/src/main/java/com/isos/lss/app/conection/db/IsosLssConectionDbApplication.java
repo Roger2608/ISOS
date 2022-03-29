0dbf0b5c404cd5cd4e39fede3cadb77f37d4a9f8
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2022-03-28 23:50:30 -0500

    commit initial

---
+++ isos_lss_conectionDB/src/main/java/com/isos/lss/app/conection/db/IsosLssConectionDbApplication.java
@@ -0,0 +1,13 @@
+package com.isos.lss.app.conection.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsosLssConectionDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsosLssConectionDbApplication.class, args);
+	}
+
+}
---
+++ isos_lss_conectionDB/src/test/java/com/isos/lss/app/conection/db/IsosLssConectionDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isos.lss.app.conection.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsosLssConectionDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
